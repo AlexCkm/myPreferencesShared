482bcb279f5865b8b1b990f0de69f8af348946db
Alumno <alex@gmail.com>
2022-07-07 20:32:17 +0200

    commit preferencias

--- app/src/main/java/com/example/mysharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/mysharedpreferences/MainActivity.kt
@@ -3,20 +3,26 @@ package com.example.mysharedpreferences
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import com.example.mysharedpreferences.SharedPreferences.Companion.prefs
 import com.example.mysharedpreferences.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var opColor = "Color"
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
+        getSpinner(binding.spinner)
         initUI()
-        checkUserValues()
+        chesckUserValues()
     }
+
     fun initUI(){
         binding.btnEntrar.setOnClickListener {
             accessSharedPreferences()
@@ -24,23 +30,53 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-    private fun checkUserValues(){
-        }
+
     fun chesckUserValues(){
         if (prefs.getName().isNotEmpty()){
             goAccess()
          }
     }
+
     fun accessSharedPreferences(){
         if (binding.etNombre.text.toString().isNotEmpty()){
             prefs.saveName(binding.etNombre.text.toString())
             prefs.saveColor(binding.cbColor.isChecked)
+            prefs.saveSpinner()
             goAccess()
         }else {
             Toast.makeText(this, "Rellene este parametro", Toast.LENGTH_LONG).show()
         }
     }
+
     private fun goAccess(){
         startActivity(Intent(this, NextActivity::class.java))
+    }
+
+    fun getSpinner(spinner: Spinner){
+
+        val adaptador: ArrayAdapter<*> = ArrayAdapter.createFromResource(this, R.array.Colores,
+            android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adaptador
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                opColor = parent?.getItemAtPosition(position).toString()
+                msj(opColor)
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                opColor = "Rojo"
+
+            }
+        }
+    }
+
+    fun msj(str:String){
+        Toast.makeText(this@MainActivity, str, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/mysharedpreferences/Prefs.kt
+++ app/src/main/java/com/example/mysharedpreferences/Prefs.kt
@@ -2,26 +2,36 @@ package com.example.mysharedpreferences
 
 import android.content.Context
 import android.graphics.Color
+import android.widget.Spinner
 
 class Prefs(val context:Context) {
     val DATABASE = "MyDB"
     val USER_NAME = "UserName"
     val Color = "Color"
+    val Spinner = "Spinner"
     val storage = context.getSharedPreferences(DATABASE,
         Context.MODE_PRIVATE)
 
     fun saveName(name:String){
         storage.edit().putString(USER_NAME, name).apply()
     }
+
     fun saveColor(color:Boolean){
         storage.edit().putBoolean(Color, color).apply()
     }
+
     fun getName():String{
         return storage.getString(USER_NAME, "")!!
     }
+
     fun getColorCheck():Boolean{
         return storage.getBoolean(Color, false)
     }
+
+    fun saveSpinner():Boolean{
+        return storage.getBoolean(Spinner,false)
+    }
+
     fun cleanData(){
         storage.edit().clear().apply()
     }
